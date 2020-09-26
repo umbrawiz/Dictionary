28da8e8d1b0212f13240d259374909636eee8b8e
umbrawiz <ruyumibabe@gmail.com>
2020-09-26 21:19:52 +0700

    Create Word.java
    .

---
+++ src/Word.java
@@ -0,0 +1,39 @@
+public class Word {
+    private String word;
+    private String kind;
+    private String speech;
+    private String meaning;
+
+    public String getWord(){
+        return word;
+    }
+
+    public String getKind(){
+        return kind;
+    }
+
+    public String getSpeech(){
+        return speech;
+    }
+
+    public String getMeaning(){
+        return meaning;
+    }
+
+    public void setWord(String newWord){
+        this.word = newWord;
+    }
+
+    public void setKind(String newKind){
+        this.word = newKind;
+    }
+
+    public void setSpeech(String newSpeech){
+        this.word = newSpeech;
+    }
+
+    public void setMeaning(String newMeaning){
+        this.word = newMeaning;
+    }
+
+}

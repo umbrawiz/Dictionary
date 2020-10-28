108496cd838f792941afeb2ab72f40fb81e28cbf
umbrawiz <ruyumibabe@gmail.com>
2020-10-28 09:21:25 +0700

    .

--- src/Translate.java
+++ src/Translate.java
@@ -22,7 +22,23 @@ public class Translate {
             response.append(inputLine);
         }
         in.close();
+        if (text.equals(response.toString())) {
+            String urlStr1 = "https://script.google.com/macros/s/AKfycbyZNzsPa3FjuL4QihNLLAeEsDRixMdih_bgkkqhexnVor67v90/exec" +
+                    "?q=" + URLEncoder.encode(text, "UTF-8") +
+                    "&target=" + langFrom +
+                    "&source=" + langTo;
+            URL url1 = new URL(urlStr1);
+            StringBuilder response1 = new StringBuilder();
+            HttpURLConnection con1 = (HttpURLConnection) url1.openConnection();
+            con1.setRequestProperty("User-Agent", "Mozilla/5.0");
+            BufferedReader in1 = new BufferedReader(new InputStreamReader(con1.getInputStream()));
+            String inputLine1;
+            while ((inputLine1 = in1.readLine()) != null) {
+                response1.append(inputLine1);
+            }
+            in1.close();
+            return response1.toString();
+        }
         return response.toString();
     }
-
 }
